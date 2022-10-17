8345d3d2c98883a1cbe012c4559d6fb9f019b278
thetommedia@gmail.com <thetommedia@gmail.com>
2022-10-17 22:23:36 +0100

    Yes

--- config/keys.js
+++ config/keys.js
@@ -1,2 +1,4 @@
-export const MAIL_JET_API_KEY = '37e885c3ff6ec0b8657b7210432cef55';
-export const MAIL_JET_SECRET_KEY = '3c7c01e404e6995142c762e56a4e16e7';
+// export const MAIL_JET_API_KEY = '37e885c3ff6ec0b8657b7210432cef55';
+// export const MAIL_JET_SECRET_KEY = '3c7c01e404e6995142c762e56a4e16e7';
+export const MAIL_JET_API_KEY = 'ea673cf2d97cd4a09b62f482a29f407e';
+export const MAIL_JET_SECRET_KEY = '83f20cf2e41ccaefa4079d18b4cd4c2a';
